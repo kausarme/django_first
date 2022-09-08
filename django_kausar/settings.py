811308e1bd075465130cc85e02014fd71864b6b0
kausarme <67036164+kausarme@users.noreply.github.com>
2022-09-08 10:34:26 +0700

    Update settings.py

--- django_kausar/settings.py
+++ django_kausar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-7)*+9!a%rc0f3i9+bbmtbjl#vwgm4l551djran1q@bm+%=6^m1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
